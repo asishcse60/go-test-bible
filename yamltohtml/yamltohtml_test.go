da9b81c86a46ce8a264876c5f399bb022f762973
asish_orbitax <asish.pal@orbitax.com>
2021-07-29 22:41:57 +0600

    test with TestMain

--- yamltohtml/yamltohtml_test.go
+++ yamltohtml/yamltohtml_test.go
@@ -1,11 +1,22 @@
 package yamltohtml
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
 
 type TestCase struct {
 	desc     string
 	path     string
 	expected string
+}
+
+func TestMain(m *testing.M){
+	fmt.Println("Hello World")
+	ret := m.Run()
+	fmt.Println("Tests have executed")
+	os.Exit(ret)
 }
 
 func TestYamlToHTML(t *testing.T) {
