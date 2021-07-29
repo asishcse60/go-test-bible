edfd72dfc3ff1da823c5a84cfc04b5da4cf65a94
asish_orbitax <asish.pal@orbitax.com>
2021-07-30 00:48:03 +0600

    httppoint test

--- main.go
+++ main.go
@@ -3,8 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/asishcse60/go-test-bible/calculator"
+	"log"
+	"net/http"
 )
 
 func main() {
+	Setup()
 	fmt.Println(calculator.CalculateIsArmstrong(371))
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func Setup() {
+	http.HandleFunc("/", HomePage)
+}
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Welcome to homepage")
+	fmt.Println("Homepage endpoints hits.")
 }
---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHomePage(t *testing.T){
+	req := httptest.NewRequest("GET","/", nil)
+	ww := httptest.NewRecorder()
+	handler := http.HandlerFunc(HomePage)
+	handler.ServeHTTP(ww, req)
+	assert.Equal(t, http.StatusOK, ww.Code)
+}
